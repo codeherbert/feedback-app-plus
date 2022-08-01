297384e737bd7be940161172aff67e1239d5031d
tech master <bryan77@twc.com>
2022-08-01 08:17:22 -0400

    creates a feedbackitem component with initial state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
+import FeedbackItem from './components/FeedbackItem';
 
 function App() {
   return (    
     <>
       <Header />
-      <h2>App Contents</h2>
+      <FeedbackItem />
     </>
   );
 }
