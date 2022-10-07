82c4c4d1c91ae2417afb55d5f17b67cb4f414ae7
tech master <bryan77@twc.com>
2022-10-07 09:07:11 -0400

    adds defaultProps and propTypes to Button component

--- src/components/shared/Button.js
+++ src/components/shared/Button.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Button({ children, version, type, isDisabled }) {
     return (
@@ -7,5 +8,18 @@ function Button({ children, version, type, isDisabled }) {
         </button>
     )
 };
+
+Button.defaultProps = {
+    version: 'primary',
+    type: 'button',
+    isDisabled: false
+}
+
+Button.propTypes = {
+    children: PropTypes.node.isRequired,
+    version: PropTypes.string,
+    type: PropTypes.string,
+    isDisabled: PropTypes.bool,
+}
 
 export default Button;
