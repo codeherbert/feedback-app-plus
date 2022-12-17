39527ec7d2694dc8037d704909116de798ba32b0
tech master <bryan77@twc.com>
2022-12-17 18:20:06 -0500

    adds handleSubmit to FeedbackForm and passes data to App

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import FeedbackForm from './components/FeedbackForm';
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
+  const addFeedback = (newFeedback) => {
+      setFeedback([newFeedback, ...feedback]);
+  }
+
   const deleteFeedback = (id) => {
     if(window.confirm("Are you sure you want to delete this item?")) {
       setFeedback(feedback.filter((item) => item.id !== id))
@@ -18,7 +22,7 @@ function App() {
     <>
       <Header />
       <div className="container">
-        <FeedbackForm />
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
