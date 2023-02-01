719ab68bfe42c825d8d74d2adfb0389b3a888b63
tech master <bryan77@twc.com>
2023-02-01 08:56:17 -0500

    adds about link and content for about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import Card from '../components/shared/Card';
+import { Link } from 'react-router-dom';
 
 function AboutPage () {
     return (
-        <div>About Page</div>
+        <div className="AboutPage container">
+            <Card>
+                <h1>About Page</h1>
+                <p className="info-one">This is a React app that allows feedback for a product or service.</p>
+                <p className="info-two">The user can add a comment along with a rating of 1-10. The comments can also be deleted.</p>
+                <p className="info-version">Version: 1.0</p>
+                <Link to='/'>Back to Home</Link>
+            </Card>
+        </div>
     )
 }
 
