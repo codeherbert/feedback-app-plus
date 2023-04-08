c6683c9f417e217d9c05507468fc39d02e1af9e9
tech master <bryan77@twc.com>
2023-04-08 18:47:57 -0400

    refactors addFeedback function to be used with context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import {v4 as uuidv4} from 'uuid';
-import React, {useState} from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
-import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import AboutPage from './pages/AboutPage';
@@ -11,13 +9,6 @@ import AboutIconLink from './components/AboutIconLink';
 import {FeedbackProvider} from './context/FeedbackContext';
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
-
-  const addFeedback = (newFeedback) => {
-      newFeedback.id = uuidv4();
-      setFeedback([newFeedback, ...feedback]);
-  }
-
   return (        
     <FeedbackProvider>
       <Router>
@@ -26,7 +17,7 @@ function App() {
           <Routes>
             <Route exact path='/' element={
               <>             
-                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackForm />
                 <FeedbackStats />
                 <FeedbackList />
               </>
