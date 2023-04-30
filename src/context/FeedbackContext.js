0ea978962420421e583f2907b0be1f077eba509c
tech master <bryan77@twc.com>
2023-04-30 00:04:26 -0400

    use effect for feedbackform and ratings edit

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -52,7 +52,8 @@ export const FeedbackProvider = ({children}) => {
         feedback,
         addFeedback,
         deleteFeedback,
-        editFeedback
+        editFeedback,
+        feedbackEdit,
     }}>
         {children}
     </FeedbackContext.Provider>
