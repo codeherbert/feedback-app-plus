b68f8245058dfbaa4506a69217d722d0607741d5
tech master <bryan77@twc.com>
2023-06-01 11:34:29 -0400

    configures app to allow submission for updated feedback item

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -10,12 +10,14 @@ function FeedbackForm() {
     const [btnDisabled, setBtnDisabled] = useState(true);
     const [message, setMessage] = useState('');
 
-    const { addFeedback, feedbackEdit } = useContext(FeedbackContext);
+    const { addFeedback, feedbackEdit, updateFeedback } = useContext(FeedbackContext);
 
     useEffect(() => {
-        setBtnDisabled(false);
-        setText(feedbackEdit.item.text);
-        setRating(feedbackEdit.item.rating);
+        if(feedbackEdit.edit === true) {
+            setBtnDisabled(false);
+            setText(feedbackEdit.item.text);
+            setRating(feedbackEdit.item.rating);
+        }
     }, [feedbackEdit]);
 
     const handleTextChange = (e) => {
@@ -42,7 +44,14 @@ function FeedbackForm() {
                 text,
                 rating,
             }
-            addFeedback(newFeedback);
+
+            if(feedbackEdit.edit === true) {
+                updateFeedback(feedbackEdit.item.id, newFeedback);
+                
+            }
+            else {
+                addFeedback(newFeedback);    
+            }            
 
             setText('');
         }
